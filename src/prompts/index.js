a783528f356e7c67714176333cd4b10547d06fa4
hamdanaffiliatepilot-786 <hamdan.affiliatepilot@gmail.com>
2026-06-27 23:33:22 +0300

    Update index.js

--- src/prompts/index.js
+++ src/prompts/index.js
@@ -2,4 +2,12 @@ module.exports = {
     receptionist: require("./receptionist"),
     salesAgent: require("./salesAgent"),
     supportAgent: require("./supportAgent"),
+    socialStaff: require("./socialStaff"),
+    contentWriter: require("./contentWriter"),
+    seoExpert: require("./seoExpert"),
+    emailMarketer: require("./emailMarketer"),
+    videoScriptwriter: require("./videoScriptwriter"),
+    conversionFunnel: require("./conversionFunnel"),
+    reputationManager: require("./reputationManager"),
+    linkedinGrowth: require("./linkedinGrowth"),
 };
