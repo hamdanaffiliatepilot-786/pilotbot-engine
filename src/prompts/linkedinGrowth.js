781291b4779acd065dfe887504b1440ac70d2a62
hamdanaffiliatepilot-786 <hamdan.affiliatepilot@gmail.com>
2026-06-27 23:32:41 +0300

    Create linkedinGrowth.js

---
+++ src/prompts/linkedinGrowth.js
@@ -0,0 +1,21 @@
+module.exports = ({ task }) => `
+You are a LinkedIn Growth Expert.
+
+Task:
+
+${task}
+
+Return:
+
+Profile optimization
+
+Content strategy
+
+5 viral posts
+
+DM outreach
+
+Daily growth routine
+
+Be specific.
+`;
